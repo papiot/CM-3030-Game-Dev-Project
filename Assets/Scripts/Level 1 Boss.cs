cdea6d0104d1acdd7face2210f1812cc837e547e
BeetleZeus <oelshark@gmail.com>
2024-07-29 20:53:29 -0700

    Level 1 Boss Mechanics + Debugging
    Level 1 Boss script updated to improve animation & fight mechanics.
    
    * logic added to make boss turn a shade of red to inform player that boss is vulnerable
    *NavMesh Agent parameters updated to enhance speed & fight mechanics
    * boolean checks added to the script for fine tuned control of specific animation transitions (isRoaring, isVunlerable, isDead)
    * SFX added for level cleared when boss is defeated
    
    *Debugging_PlayerHealth script was created for debugging purposes to ensure player hits & boss hits are registering properly

--- Assets/Scripts/Level 1 Boss.cs
+++ Assets/Scripts/Level 1 Boss.cs
@@ -8,28 +8,31 @@ public class Level1Boss : MonoBehaviour
     // variables for player posisiton & Boss movement
     private Transform player; // Reference to the player
     [SerializeField] LayerMask playerMask;
-    [SerializeField] float speed = 12f; // Speed at which the boss runs
-    [SerializeField] float jumpForce = 10f; // Force applied for the jump attack
     [SerializeField] NavMeshAgent bossAgent = null;
     [SerializeField] Animator animator = null; // Reference to the Animator component
     private Rigidbody rb; // Reference to the Rigidbody component
 
     //variables for boss attacks
-    // enemy attaking
     [SerializeField] float detectionRange = 20f;
     [SerializeField] float attackRange = 3f;
     private bool isPlayerInDetectionRange;
     private bool isPlayerInAttackRange;
     private bool isAttacked;
-    [SerializeField] float attackCooldown = 2f;
+    [SerializeField] float attackCooldown = 4f;
+    private bool bossVulnerable = false;
 
-
-    private bool isJumping = false; // Flag to indicate if the boss is currently jumping
-    private bool isPunching = false; // Flag to indicate if the boss is currently punching
-    private bool isClawSwiping = false; // Flag to indicate if the boss is currently doing the claw swipe attack
+    private float introTimer = 0f;
+    private bool isRoaring = true;
 
     [SerializeField] int health = 30;
     [SerializeField] int damagePerHit = 1;
+    [SerializeField] GameObject vulnerableMesh;
+
+    [SerializeField] GameObject finishPoint;
+    [SerializeField] AudioSource playAudio;
+    [SerializeField] AudioSource levelClearAudio;
+
+    private Debugging_PlayerHealth playerHealth;
 
     private void Awake()
     {
@@ -37,6 +40,8 @@ public class Level1Boss : MonoBehaviour
         rb = GetComponent<Rigidbody>();
         if (bossAgent == null) bossAgent = GetComponent<NavMeshAgent>();
         if(animator == null) animator = GetComponentInChildren<Animator>();
+
+        playerHealth = GameObject.Find("Player").GetComponent<Debugging_PlayerHealth>();
     }
 
     void Update()
@@ -51,13 +56,32 @@ public class Level1Boss : MonoBehaviour
         {
             CheckForAttack();
         }
+
+        introTimer += Time.deltaTime;
+        if(introTimer >= 4f)
+        {
+            isRoaring = false;
+            animator.SetBool("Is_Roaring", false);
+        }
+
+        if (bossVulnerable)
+        {
+            vulnerableMesh.SetActive(true);
+        }
+        else if (!bossVulnerable)
+        {
+            vulnerableMesh.SetActive(false);
+        }
     }
 
     private void RunTowardsPlayer()
     {
-        if (bossAgent.isActiveAndEnabled && !isAttacked)
+        if (!isRoaring && !isAttacked)
         {
-            bossAgent.SetDestination(player.position);
+            if (bossAgent.isActiveAndEnabled && !isAttacked)
+            {
+                bossAgent.SetDestination(player.position);
+            }
         }
     }
 
@@ -81,22 +105,33 @@ public class Level1Boss : MonoBehaviour
             }
 
             isAttacked = true;
+            bossVulnerable = true;
             Invoke("ResetAttack", attackCooldown);
         }
     }
 
     private void ResetAttack()
     {
+        bossVulnerable = false;
         isAttacked = false;
     }
 
     public void TakeDamage(int damage)
     {
-        health -= damage;
+        if (bossVulnerable)
+        {
+            health -= damage;
+            Debug.Log("Boss Hit");
+        }
 
         if(health <= 0)
         {
-            Invoke("DestroyEnemy", 1f);
+            animator.SetBool("Is_Dead", true);
+            bossAgent.isStopped = true;
+            playAudio.Stop();
+            levelClearAudio.Play();
+            finishPoint.SetActive(true);
+            //Invoke("DestroyEnemy", 10f);
         }
     }
 
@@ -107,6 +142,15 @@ public class Level1Boss : MonoBehaviour
 
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            // Handle damage to the player here
+            playerHealth.health -= 2;
+            Debug.Log("Player Hit. Player Health: " + playerHealth.health);
+        }
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
@@ -115,65 +159,6 @@ public class Level1Boss : MonoBehaviour
             TakeDamage(damagePerHit);
             Destroy(collision.gameObject);
         }
-
-        if (isJumping && collision.gameObject.CompareTag("Player"))
-        {
-            // Handle damage to the player here
-            Debug.Log("Boss hit the player with jump attack!");
-        }
-
-        if (isPunching && collision.gameObject.CompareTag("Player"))
-        {
-            // Handle damage to the player here
-            Debug.Log("Boss hit the player with punch attack!");
-        }
-
-        if (isClawSwiping && collision.gameObject.CompareTag("Player"))
-        {
-            // Handle damage to the player here
-            Debug.Log("Boss hit the player with claw swipe attack!");
-        }
     }
 
-
-
-    // This method is called by an animation event at the start of the jump attack animation
-    public void StartJumpAttack()
-    {
-        isJumping = true;
-        Vector3 direction = (player.position - transform.position).normalized;
-        rb.AddForce(new Vector3(direction.x, 1, direction.z) * jumpForce, ForceMode.VelocityChange);
-    }
-
-    // This method is called by an animation event at the end of the jump attack animation
-    public void EndJumpAttack()
-    {
-        isJumping = false;
-    }
-
-    // This method is called by an animation event at the start of the punching animation
-    public void StartPunching()
-    {
-        isPunching = true;
-    }
-
-    // This method is called by an animation event at the end of the punching animation
-    public void EndPunching()
-    {
-        isPunching = false;
-        // Automatically transition to Claw Swipe after Punching
-        animator.SetTrigger("Attack3"); // Trigger the claw swipe animation
-    }
-
-    // This method is called by an animation event at the start of the claw swipe animation
-    public void StartClawSwipe()
-    {
-        isClawSwiping = true;
-    }
-
-    // This method is called by an animation event at the end of the claw swipe animation
-    public void EndClawSwipe()
-    {
-        isClawSwiping = false;
-    }
 }
