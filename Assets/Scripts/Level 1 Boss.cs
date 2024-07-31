bf01113325a0b33f47e8433d263981b4aa2816b1
BeetleZeus <oelshark@gmail.com>
2024-07-31 10:34:06 -0700

    Revert "Merge pull request #24 from papiot/Cam_Controller"
    This reverts commit b05b94a33ed195b47e9a2fb8f2f77fac8af0995e, reversing
    changes made to f1ee7a7ede43b1a08898220915f3b3a49bb916fc.

--- Assets/Scripts/Level 1 Boss.cs
+++ Assets/Scripts/Level 1 Boss.cs
@@ -18,7 +18,6 @@ public class Level1Boss : MonoBehaviour
     private bool isPlayerInDetectionRange;
     private bool isPlayerInAttackRange;
     private bool isAttacked;
-    private bool isDead = false;
     [SerializeField] float attackCooldown = 4f;
 
     private float introTimer = 0f;
@@ -88,7 +87,7 @@ public class Level1Boss : MonoBehaviour
 
     private void RunTowardsPlayer()
     {
-        if (!isRoaring && !isAttacked && !isIdle && !isDead)
+        if (!isRoaring && !isAttacked && !isIdle)
         {
             if (bossAgent.isActiveAndEnabled && !isAttacked)
             {
@@ -99,11 +98,8 @@ public class Level1Boss : MonoBehaviour
 
     private void CheckForAttack()
     {
-        if (!isDead)
-        {
-            bossAgent.SetDestination(transform.position);
-            transform.LookAt(player);
-        }
+        bossAgent.SetDestination(transform.position);
+        transform.LookAt(player);
         // Randomly choose between Jump Attack and Punch/Swipe Attack
         int attackChoice = Random.Range(0, 10); // Randomly choose between 0 and 9
 
@@ -154,26 +150,25 @@ public class Level1Boss : MonoBehaviour
 
     private void BossDeathSequence()
     {
-        isDead = true;
         bossSFX.PlayOneShot(bossIntroDeathClip);
         bossAgent.enabled = false;
         animator.Play("Boss Dying");
         playAudio.Stop();
-        //finishPoint.SetActive(true);
+        levelClearAudio.Play();
+        finishPoint.SetActive(true);
         bossDeathEffect.Play();
-        Invoke("DestroyEnemy", 5f);
+        Invoke("DestroyEnemy", 7f);
     }
 
     private void DestroyEnemy()
     {
         Destroy(gameObject);
-        finishPoint.SetActive(true);
-        levelClearAudio.Play();
+
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.tag == "Player" && !isIdle && !isDead)
+        if(other.gameObject.tag == "Player")
         {
             // Handle damage to the player here
             playerHealth.health -= 2;
