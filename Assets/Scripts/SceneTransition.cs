7f4904a5db4bf319f9a5fde8eedd6e594fa8ea53
Ark Roy <ark.roy@gmail.com>
2024-09-01 11:32:44 -0400

    Updated sceneTransition

--- Assets/Scripts/SceneTransition.cs
+++ Assets/Scripts/SceneTransition.cs
@@ -13,7 +13,6 @@ public class SceneTransition : MonoBehaviour
         sceneManager = GameObject.Find("Scene Manager").GetComponent<SceneTransition>();
     }
 
-
     public void LoadNextLevel()
     {
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
@@ -48,31 +47,40 @@ public class SceneTransition : MonoBehaviour
 
     public void PauseUnpauseScene()
     {
-        if(Time.timeScale == 1)
+        if (Time.timeScale == 1)
         {
             Time.timeScale = 0;
             // create a screen overlay for paused game in Unity Editor.
         }
-        else if(Time.timeScale == 0)
+        else if (Time.timeScale == 0)
         {
             Time.timeScale = 1;
         }
-        
+
     }
+
     public void StartCampaign()
     {
-        GameManager.Instance.SetCampaignMode(true);
+        GameManager.Instance.SetCampaignMode(true);  // Enable campaign mode
         SceneManager.LoadScene("1_Level 1"); // Load Level 1 for Start Campaign
     }
 
     public void LoadLevel1()
     {
+        GameManager.Instance.SetCampaignMode(false); // Disable campaign mode
         SceneManager.LoadScene("1_Level 1"); // Load Level 1 for Start Level 1
     }
 
     public void LoadLevel2()
     {
-        SceneManager.LoadScene("2_Level 2"); // Load Level 1 for Start Level 2
+        GameManager.Instance.SetCampaignMode(false); // Disable campaign mode
+        SceneManager.LoadScene("2_Level 2"); // Load Level 2 for Start Level 2
     }
 
+    public void QuitToMainMenu()
+    {
+        GameManager.Instance.ResetEnemiesKilled();  // Reset enemies killed when quitting to main menu
+        GameManager.Instance.ResetCoinsCollected(); // Reset coins collected when quitting to main menu
+        SceneManager.LoadScene("0_IntroScreen");
+    }
 }
